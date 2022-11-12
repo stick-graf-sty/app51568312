f09742fb1de492438927091e2a1e0a2a17e571df
Azamk414 <azam.rabota@mail.ru>
2022-11-12 20:57:58 +0300

    first commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f23c587e20c946717146bb69483583f5.js"
+  "./precache-manifest.e0cf6bac34d01b044d6a948f16fbee4a.js"
 );
 
 self.addEventListener('message', (event) => {
