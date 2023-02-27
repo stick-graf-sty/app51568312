9d1c0373ec85b0d62890f05093a0d3426fb8c28e
Azamk414 <ilya.steik@mail.ru>
2023-02-27 18:40:24 +0300

    first commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4ac8140d7fada48c58c72614a8923a15.js"
+  "./precache-manifest.b69577b226e062f843275ea09827499c.js"
 );
 
 self.addEventListener('message', (event) => {
